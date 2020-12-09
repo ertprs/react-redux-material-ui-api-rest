c38c924f934ddb91a9199956000cb6d59559a563
renanmoura6941 <naneraruom@gmail.com>
2020-12-09 17:04:01 -0300

    set state

--- fundaments/started-react/src/App.js
+++ fundaments/started-react/src/App.js
@@ -4,22 +4,32 @@ import React from 'react';
 //formas  de criar componete react
 
 
-function AppFuncional(){
+function AppFuncional() {
   return (
     <h1>My name is Renan moura</h1>
   );
 }
 
-class App extends React.Component{
+class App extends React.Component {
 
   state = {
-    name:'Renan moura'
+    name: 'Renan moura'
+  }
+  
+ changeName = (event)=>{
+
+    this.setState({
+      name:event.target.value
+    })
   }
 
 
-  render(){
-    return(
-    <h1>What is your name?{this.state.name}</h1>
+  render() {
+    return (
+      <>
+        <input type="text" value={this.state.nome} onChange={this.changeName} />
+        <h1>What is your name?{this.state.name}</h1>
+      </>
     )
   }
 }
