c6d7b4799f1b54dc30cf2386e06c4bf98dff30c1
renanmoura6941 <naneraruom@gmail.com>
2020-12-10 09:10:24 -0300

    component

--- fundaments/started-react/src/App.js
+++ fundaments/started-react/src/App.js
@@ -15,20 +15,42 @@ class App extends React.Component {
   state = {
     name: 'Renan moura'
   }
-  
- changeName = (event)=>{
 
+  changeName = (event) => {
     this.setState({
-      name:event.target.value
+      name: event.target.value
     })
   }
 
+  createComboBox = () => {
 
-  render() {
+    const options = ["one", "two", "three", "four"];
+    const comboBox = options.map(option => <option>{option}</option>);
+
+    return (
+      <select>
+        {comboBox}
+      </select>
+    )
+  }
+  
+  componentDidMount(){
+    console.log("didMount")
+  }
+
+
+  render() {   
+     console.log("render...")
+
+    const MyComboBox = () => this.createComboBox();
     return (
       <>
         <input type="text" value={this.state.nome} onChange={this.changeName} />
         <h1>What is your name?{this.state.name}</h1>
+        <h2>{this.props.name}</h2>
+        
+        <MyComboBox />
+        {this.createComboBox()}
       </>
     )
   }
