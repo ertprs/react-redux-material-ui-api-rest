feb8e53e78a56bcf6b15491b65ed0373f746c85c
renanmoura6941 <naneraruom@gmail.com>
2020-12-14 00:55:39 -0300

    routs and screen table consult products

--- fundaments/product-registration/src/routs.js
+++ fundaments/product-registration/src/routs.js
@@ -5,6 +5,8 @@ import {HashRouter, Switch, Route} from 'react-router-dom'
 import Home from './views/home'
 import RegisterProduct from './views/products/register'
 
+import Query from './views/products/query'
+
 
 export default ()=>{
     return (
@@ -12,6 +14,8 @@ export default ()=>{
             <Switch>
                 <Route exact path="/register-products" component={RegisterProduct}/>
                 <Route exact path="/" component={Home}/>
+                <Route exact path="/query" component={Query}/>
+
             </Switch>
         </HashRouter>
     );
--- fundaments/product-registration/src/views/products/query.jsx
+++ fundaments/product-registration/src/views/products/query.jsx
@@ -1,10 +1,49 @@
 import { render } from '@testing-library/react'
 import React from 'react'
 
-export default class Query extends React.component{
+export default class Query extends React.Component {
+    state = {
+        products: []
+    }
 
-    render(){
-        return (<></>)
+    render() {
+        return (<>
+            <div className="card-header">Query products</div>
+            <div className="card-body">
+
+                <table className=" table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>SKU</th>
+                            <th>price</th>
+                            <th>Provider</th>
+                            <th></th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.products.map(product => {
+                                return
+                                <tr>
+                                    <th>product.name</th>
+                                    <th>product.sku</th>
+                                    <th>product.price</th>
+                                    <th>product.provider</th>
+                                    <th></th>
+                                </tr>
+                            })
+                        }
+                    </tbody>
+
+                </table>
+
+
+
+            </div>
+
+        </>)
     }
 
 }
