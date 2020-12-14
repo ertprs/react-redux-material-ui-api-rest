0d69ce292e1ad403be885c0f22e331ebe8e4567e
renanmoura6941 <naneraruom@gmail.com>
2020-12-14 00:32:33 -0300

    starting query screen

---
+++ fundaments/product-registration/src/views/products/query.jsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+
+export default class Query extends React.component{
+
+    render(){
+        return (<></>)
+    }
+
+}
